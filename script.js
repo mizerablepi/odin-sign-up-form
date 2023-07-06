d47689ebdd37a57f50afcf6aa6c0ce3a2306428e
miz <mizerablepi@gmail.com>
2023-07-06 23:29:56 +0530

    complete bsic

---
+++ script.js
@@ -0,0 +1,8 @@
+let passwordAlert = document.getElementById('wrng-p');
+let submit 
+
+function alertWrongPassword() {
+  passwordAlert.textContent = "* Passwords do not match";
+  passwordAlert.style.paddingBottom = 0;
+}
+
