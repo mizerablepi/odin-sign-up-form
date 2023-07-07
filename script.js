315c5bd8f1604a6fb879b0d50805314957c65f8d
miz <mizerablepi@gmail.com>
2023-07-07 19:14:18 +0530

    fin.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let passwordAlert = document.getElementById('wrng-p');
-let submit = document.getElementById('submit')
+let password = document.getElementById('password');
+let passwordConfirmation = document.getElementById('password-c');
 
-function alertWrongPassword() {
-  passwordAlert.textContent = "* Passwords do not match";
-  passwordAlert.style.paddingBottom = 0;
+password.addEventListener('input', checkPassword);
+passwordConfirmation.addEventListener('input', checkPassword);
+
+function checkPassword() {
+  if (passwordConfirmation.value !== '') {
+    setAlert(password.value === passwordConfirmation.value);
+  }
 }
 
+function setAlert(passwordSame) {
+  passwordAlert.textContent = passwordSame ? '' : "* Passwords do not match";
+  passwordAlert.style.paddingBottom = passwordSame ? '0.94rem' : 0;
+  password.style.borderColor = passwordSame ? 'green' : 'red';
+  passwordConfirmation.style.borderColor = passwordSame ? 'green': 'red';
+}
+
+function setBorder(passwordSame) {
+  
+}
